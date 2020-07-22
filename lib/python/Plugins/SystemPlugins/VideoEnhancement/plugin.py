dff3e79f6bf9bbd0bd9bffe08c55599ed113c06d
teamblue-e2 <teamblue@online.de>
2020-07-22 09:53:55 +0200

    ***   File "./lib/python/Plugins/SystemPlugins/VideoEnhancement/plugin.py", line 3     from __future__ import print_function     ^ SyntaxError: from __future__ imports must occur at the beginning of the file

--- lib/python/Plugins/SystemPlugins/VideoEnhancement/plugin.py
+++ lib/python/Plugins/SystemPlugins/VideoEnhancement/plugin.py
@@ -1,6 +1,5 @@
-import os
-
 from __future__ import print_function
+import os
 from Plugins.Plugin import PluginDescriptor
 from Components.ConfigList import ConfigListScreen
 from Components.config import getConfigListEntry, config, ConfigNothing
