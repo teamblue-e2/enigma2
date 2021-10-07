3ec1e7a9bf778f85c511ff9a54f037daaa922d8d
teamblue-e2 <teamblue@online.de>
2021-10-07 09:56:46 +0200

    Py 3 fixes

--- lib/python/Screens/InputDeviceSetup.py
+++ lib/python/Screens/InputDeviceSetup.py
@@ -201,7 +201,7 @@ class InputDeviceSetup(Screen, ConfigListScreen):
 		exec(cmd)
 		label = _("Delay before key repeat starts:")
 		cmd = "self.delayEntry = getConfigListEntry(label, config.inputDevices." + self.inputDevice + ".delay)"
-		exec cmd
+		exec(cmd)
 		label = _("Device name:")
 		cmd = "self.nameEntry = getConfigListEntry(label, config.inputDevices." + self.inputDevice + ".name)"
 		exec(cmd)
