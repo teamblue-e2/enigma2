02c390807213b5312ac11280823e53ec876d5229
teamblue-e2 <teamblue@online.de>
2021-10-07 10:51:22 +0200

    Python2 compatibility

--- lib/python/Plugins/SystemPlugins/PositionerSetup/ui.py
+++ lib/python/Plugins/SystemPlugins/PositionerSetup/ui.py
@@ -1,3 +1,6 @@
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
 from enigma import eTimer, eDVBSatelliteEquipmentControl, eDVBResourceManager, eDVBDiseqcCommand, eDVBFrontendParametersSatellite, iDVBFrontend
 
 from Screens.Screen import Screen
