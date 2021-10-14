b404b07156fd6066f6e55e67a198b0c103bd0d18
teamblue-e2 <teamblue@online.de>
2021-10-14 13:41:23 +0200

    Fix import

--- lib/python/Components/Converter/PliExtraInfo.py
+++ lib/python/Components/Converter/PliExtraInfo.py
@@ -8,7 +8,7 @@ from Components.Element import cached
 from Components.config import config
 from Tools.Transponder import ConvertToHumanReadable
 from Tools.GetEcmInfo import GetEcmInfo
-from Poll import Poll
+from Components.Comverter.Poll import Poll
 from skin import parameters
 import six
 
