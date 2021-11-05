d2f94af42552d48342b8d288a92aafca45a65047
AbuBaniaz <AbuBaniaz@users.noreply.github.com>
2021-11-05 12:41:52 +0000

    [Usageconfig] Language selection (#3170)
    * [UsageConfig.py] Language: "qaa" is not English
    This code is used for original language and is duplicated in that section.
    
    By Huevos
    https://github.com/OpenViX/enigma2/commit/81b4e71f858e80c5438a026c6966e3e0252ea30b
    
    * UsageConfig] Language: add "oth" to "Original"
    Used by Rai (Italian national channels)
    
    By Huevos
    https://github.com/OpenViX/enigma2/commit/c671341af2100410feb490a2da9a4ee614cac3c4
    
    * [UsageConfig] Add "Visual impaired commentary"
    to auto language selection.
    
    By Huevos
    https://github.com/OpenViX/enigma2/commit/9ce0404daf339a7a06a16d911a86201c6dcbb791
    
    * [UsageConfig] Language: add "qad" to "Visual impaired commentary"
    Used by French national channels.
    
    By Huevos
    https://github.com/OpenViX/enigma2/commit/7b402fd72b645a0e8f9ea226a57e63ebf37c9e91
    
    (cherry picked from commit 39ee3181e9ce412d24eccc7a5b1e0a7d222953e0)

--- lib/python/Components/UsageConfig.py
+++ lib/python/Components/UsageConfig.py
@@ -726,7 +726,7 @@ def InitUsageConfig():
 	config.autolanguage = ConfigSubsection()
 	audio_language_choices = [
 		("", _("None")),
-		("orj dos ory org esl qaa und mis mul ORY ORJ Audio_ORJ", _("Original")),
+		("orj dos ory org esl qaa und mis mul ORY ORJ Audio_ORJ oth", _("Original")),
 		("ara", _("Arabic")),
 		("eus baq", _("Basque")),
 		("bul", _("Bulgarian")),
@@ -736,7 +736,7 @@ def InitUsageConfig():
 		("ces cze", _("Czech")),
 		("dan", _("Danish")),
 		("dut ndl nld", _("Dutch")),
-		("eng qaa", _("English")),
+		("eng", _("English")),
 		("est", _("Estonian")),
 		("fin", _("Finnish")),
 		("fra fre", _("French")),
@@ -762,7 +762,7 @@ def InitUsageConfig():
 		("tha", _("Thai")),
 		("tur Audio_TUR", _("Turkish")),
 		("ukr Ukr", _("Ukrainian")),
-		("NAR", _("Visual impaired commentary"))]
+		("NAR qad", _("Visual impaired commentary"))]
 
 	epg_language_choices = audio_language_choices[:1] + audio_language_choices[2:]
 
