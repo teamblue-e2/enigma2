d3391ec6747c903131bfa5cef561ddfedcbc8cc8
Madars Auns <taapat@gmail.com>
2022-01-11 21:38:06 +0200

    Use existing renderer for gb led settings (#3270)
    In the enigma from the beginning there is a renderer FrontpanelLed which is used for led control.
    Therefore, we can use it and do not need to additionally write values in the proc on startup.
    This also introduces the ability to set led profiles for use in different modes for frontpanel led control.
    
    (cherry picked from commit 27ac5778d54def65029170738e39bda64c7b4a70)

--- lib/python/Components/UsageConfig.py
+++ lib/python/Components/UsageConfig.py
@@ -3,6 +3,7 @@ from config import ConfigSubsection, ConfigYesNo, config, ConfigSelection, Confi
 from Tools.Directories import defaultRecordingLocation
 from enigma import setTunerTypePriorityOrder, setPreferredTuner, setSpinnerOnOff, setEnableTtCachingOnOff, eEnv, eDVBDB, Misc_Options, eBackgroundFileEraser, eServiceEvent
 from Components.NimManager import nimmanager
+from Components.Renderer.FrontpanelLed import ledPatterns, PATTERN_ON, PATTERN_OFF, PATTERN_BLINK
 from Components.ServiceList import refreshServiceList
 from SystemInfo import SystemInfo
 
@@ -12,18 +13,6 @@ import time
 
 originalAudioTracks = "orj dos ory org esl qaa und mis mul ORY ORJ Audio_ORJ oth"
 visuallyImpairedCommentary = "NAR qad"
-
-
-def leaveStandby():
-	if not config.usage.powerLED.value:
-		open(SystemInfo["PowerLED"], "w").write("0")
-
-
-def standbyCounterChanged(dummy):
-	from Screens.Standby import inStandby
-	inStandby.onClose.append(leaveStandby)
-	if not config.usage.standbyLED.value:
-		open(SystemInfo["StandbyLED"], "w").write("0")
 
 
 def InitUsageConfig():
@@ -380,6 +369,8 @@ def InitUsageConfig():
 		def powerLEDChanged(configElement):
 			if "fp" in SystemInfo["PowerLED"]:
 				open(SystemInfo["PowerLED"], "w").write(configElement.value and "1" or "0")
+				patterns = [PATTERN_ON, PATTERN_ON, PATTERN_OFF, PATTERN_OFF] if configElement.value else [PATTERN_OFF, PATTERN_OFF, PATTERN_OFF, PATTERN_OFF]
+				ledPatterns.setLedPatterns(1, patterns)
 			else:
 				open(SystemInfo["PowerLED"], "w").write(configElement.value and "on" or "off")
 		config.usage.powerLED = ConfigYesNo(default=True)
@@ -387,13 +378,13 @@ def InitUsageConfig():
 
 	if SystemInfo["StandbyLED"]:
 		def standbyLEDChanged(configElement):
-			open(SystemInfo["StandbyLED"], "w").write(configElement.value and "on" or "off")
+			if "fp" in SystemInfo["StandbyLED"]:
+				patterns = [PATTERN_OFF, PATTERN_BLINK, PATTERN_ON, PATTERN_BLINK] if configElement.value else [PATTERN_OFF, PATTERN_OFF, PATTERN_OFF, PATTERN_OFF]
+				ledPatterns.setLedPatterns(0, patterns)
+			else:
+				open(SystemInfo["StandbyLED"], "w").write(configElement.value and "on" or "off")
 		config.usage.standbyLED = ConfigYesNo(default=True)
-		if not "fp" in SystemInfo["StandbyLED"]:
-			config.usage.standbyLED.addNotifier(standbyLEDChanged)
-
-	if SystemInfo["PowerLED"] and "fp" in SystemInfo["PowerLED"] and not config.usage.powerLED.value or (SystemInfo["StandbyLED"] and "fp" in SystemInfo["StandbyLED"] and not config.usage.standbyLED.value):
-		config.misc.standbyCounter.addNotifier(standbyCounterChanged, initial_call=False)
+		config.usage.standbyLED.addNotifier(standbyLEDChanged)
 
 	if SystemInfo["SuspendLED"]:
 		def suspendLEDChanged(configElement):
