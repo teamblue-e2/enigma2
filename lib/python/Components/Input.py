d069eba3e89e3195d95a46ab3f04b48d528f3c97
nautilus7 <spyros.n7@gmail.com>
2022-01-24 02:22:10 +0200

    Input.py: Don't prefix unicode literals with "u"
    (cherry picked from commit d66d46b8204e6ff2e277aaa627b3e681d460a2d6)

--- lib/python/Components/Input.py
+++ lib/python/Components/Input.py
@@ -191,7 +191,7 @@ class Input(VariableText, GUIComponent, NumericalTextInput):
 			self.deleteAllChars()
 			self.allmarked = False
 		else:
-			self.insertChar(u" ", self.currPos, False, True)
+			self.insertChar(" ", self.currPos, False, True)
 			self.innerright()
 		self.update()
 
