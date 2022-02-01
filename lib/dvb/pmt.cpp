bd65bedb6f4b905a2dfed95d9b30069c4596b1c2
Athanasios Oikonomou <athoik@gmail.com>
2022-02-01 22:45:58 +0200

    Use C++11 struct initializer
    This commit initilizes (almost) all structs in enigma2.
    Some structs already fixed by 1283d41290f60bf65812f97e42418d97b38a0e34
    
    As a result, using valgrind we get almost no warnings for uninitialized values/bytes!
    
        138 ==1284== Conditional jump or move depends on uninitialised value(s)
          2 ==1284== Syscall param ioctl(FIBMAP) points to uninitialised byte(s)
         86 ==1284== Use of uninitialised value of size 4
    VS
          6 ==2803== Conditional jump or move depends on uninitialised value(s)
          2 ==2803== Use of uninitialised value of size 4
    
    (cherry picked from commit 536ea317647c5fda2e753f397e5c89c52711bf3f)

--- lib/dvb/pmt.cpp
+++ lib/dvb/pmt.cpp
@@ -494,7 +494,7 @@ void eDVBServicePMTHandler::getAITApplications(std::map<int, std::string> &aitli
 
 void eDVBServicePMTHandler::getCaIds(std::vector<int> &caids, std::vector<int> &ecmpids, std::vector<std::string> &ecmdatabytes)
 {
-	program prog;
+	program prog = {};
 
 	if (!getProgramInfo(prog))
 	{
@@ -825,7 +825,7 @@ int eDVBServicePMTHandler::getProgramInfo(program &program)
 			vpidtype = videoStream::vtMPEG2;
 		if ( cached_vpid != -1 )
 		{
-			videoStream s;
+			videoStream s = {};
 			s.pid = cached_vpid;
 			s.type = vpidtype;
 			program.videoStreams.push_back(s);
@@ -833,7 +833,7 @@ int eDVBServicePMTHandler::getProgramInfo(program &program)
 		}
 		if ( cached_apid_ac3 != -1 )
 		{
-			audioStream s;
+			audioStream s = {};
 			s.type = audioStream::atAC3;
 			s.pid = cached_apid_ac3;
 			s.rdsPid = -1;
@@ -842,7 +842,7 @@ int eDVBServicePMTHandler::getProgramInfo(program &program)
 		}
 		if ( cached_apid_ac4 != -1 )
 		{
-			audioStream s;
+			audioStream s = {};
 			s.type = audioStream::atAC4;
 			s.pid = cached_apid_ac4;
 			s.rdsPid = -1;
@@ -851,7 +851,7 @@ int eDVBServicePMTHandler::getProgramInfo(program &program)
 		}
 		if ( cached_apid_ddp != -1 )
 		{
-			audioStream s;
+			audioStream s = {};
 			s.type = audioStream::atDDP;
 			s.pid = cached_apid_ddp;
 			s.rdsPid = -1;
@@ -860,7 +860,7 @@ int eDVBServicePMTHandler::getProgramInfo(program &program)
 		}
 		if ( cached_apid_aache != -1 )
 		{
-			audioStream s;
+			audioStream s = {};
 			s.type = audioStream::atAACHE;
 			s.pid = cached_apid_aache;
 			s.rdsPid = -1;
@@ -869,7 +869,7 @@ int eDVBServicePMTHandler::getProgramInfo(program &program)
 		}
 		if ( cached_apid_aac != -1 )
 		{
-			audioStream s;
+			audioStream s = {};
 			s.type = audioStream::atAAC;
 			s.pid = cached_apid_aac;
 			s.rdsPid = -1;
@@ -878,7 +878,7 @@ int eDVBServicePMTHandler::getProgramInfo(program &program)
 		}
 		if ( cached_apid_dra != -1 )
 		{
-			audioStream s;
+			audioStream s = {};
 			s.type = audioStream::atDRA;
 			s.pid = cached_apid_dra;
 			s.rdsPid = -1;
@@ -887,7 +887,7 @@ int eDVBServicePMTHandler::getProgramInfo(program &program)
 		}
 		if ( cached_apid_mpeg != -1 )
 		{
-			audioStream s;
+			audioStream s = {};
 			s.type = audioStream::atMPEG;
 			s.pid = cached_apid_mpeg;
 			s.rdsPid = -1;
@@ -906,7 +906,7 @@ int eDVBServicePMTHandler::getProgramInfo(program &program)
 		}
 		if (subpid > 0)
 		{
-			subtitleStream s;
+			subtitleStream s = {};
 			s.pid = (subpid & 0xffff0000) >> 16;
 			if (s.pid != program.textPid)
 			{
@@ -928,7 +928,7 @@ int eDVBServicePMTHandler::getProgramInfo(program &program)
 		CAID_LIST &caids = m_service->m_ca;
 		for (CAID_LIST::iterator it(caids.begin()); it != caids.end(); ++it)
 		{
-			program::capid_pair pair;
+			program::capid_pair pair = {};
 			pair.caid = *it;
 			pair.capid = -1; // not known yet
 			pair.databytes.clear();
@@ -1089,7 +1089,7 @@ int eDVBServicePMTHandler::tuneExt(eServiceReferenceDVB &ref, ePtr<iTsSource> &s
 	{
 		if (!ref.getServiceID().get() /* incorrect sid in meta file or recordings.epl*/ )
 		{
-			eDVBTSTools tstools;
+			eDVBTSTools tstools = {};
 			bool b = source || !tstools.openFile(ref.path.c_str(), 1);
 			eWarning("[eDVBServicePMTHandler] no .meta file found, trying to find PMT pid");
 			if (source)
