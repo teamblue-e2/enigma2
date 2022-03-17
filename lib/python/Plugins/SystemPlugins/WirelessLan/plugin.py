5e199a3e7bd5e7881baf9eddc1d860c79f840be9
ahmedmoselhi <ahmedmoselhi55@gmail.com>
2022-03-17 15:54:29 +0200

    fix error in wlan

--- lib/python/Plugins/SystemPlugins/WirelessLan/plugin.py
+++ lib/python/Plugins/SystemPlugins/WirelessLan/plugin.py
@@ -14,7 +14,7 @@ from Components.Network import iNetwork
 from Plugins.Plugin import PluginDescriptor
 from Tools.Directories import resolveFilename, SCOPE_ACTIVE_SKIN
 from Tools.LoadPixmap import LoadPixmap
-from .Wlan import iWlan, iStatus, getWlanConfigName
+from .Wlan import iWlan, iStatus, getWlanConfigName, existBcmWifi
 
 
 plugin_path = eEnv.resolve("${libdir}/enigma2/python/Plugins/SystemPlugins/WirelessLan")
