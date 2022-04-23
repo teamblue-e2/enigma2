89cb0bf52d9e3b2fdb9a8fe5f3c9e3aefc164440
ahmedmoselhi <ahmedmoselhi55@gmail.com>
2022-04-23 16:13:14 +0200

    update ChannelSelection.py

--- lib/python/Screens/ChannelSelection.py
+++ lib/python/Screens/ChannelSelection.py
@@ -1656,7 +1656,7 @@ class ChannelSelectionBase(Screen):
 									service.setName("%s - %s" % (service_name, service_type))
 									self.servicelist.addService(service)
 						cur_ref = self.session.nav.getCurrentlyPlayingServiceReference()
-						sorted(self.servicelist.l)
+						self.servicelist.l.sort()
 						if cur_ref:
 							pos = self.service_types.rfind(':')
 							refstr = '%s (channelID == %08x%04x%04x) && %s ORDER BY name' % (self.service_types[:pos + 1],
