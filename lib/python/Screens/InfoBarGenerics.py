e18e49f02ddfba12fd1e61e8ea3f8f7cc2fee914
ims21 <ims@openpli.org>
2022-05-25 22:29:37 +0200

    fix for toggle pip (#3456)
    Co-authored-by: ims <ims21@users.sourceforge.net>
    (cherry picked from commit fd49f79dbbc192498f9674361d81f76f509b9c73)

--- lib/python/Screens/InfoBarGenerics.py
+++ lib/python/Screens/InfoBarGenerics.py
@@ -2595,6 +2595,7 @@ class InfoBarPiP:
 					currentServicePath = slist.getCurrentServicePath()
 					currentBouquet = slist.getRoot()
 					slist.setCurrentServicePath(self.session.pip.servicePath, doZap=False)
+				self.session.nav.stopService()
 				self.session.pip.playService(swapservice)
 				self.session.nav.playService(pipref, checkParentalControl=False, adjust=False)
 				if slist:
