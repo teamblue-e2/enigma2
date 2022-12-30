c5f233c30d7498b7911927d83d271603f4aac5b1
WanWizard <wanwizard@wanwizard.eu>
2022-12-30 17:11:32 +0000

    Auto install before Auto restore
    This solves several issues:
    - packages with conf files will create -opkg files if they already exist when installed
    - packages with runtime dependencies will fail due to missing config
    - packages with enigma config dependencies will fail due to missin config
    - packages without proper conf files no longer have their restored files overwritten
    
    (cherry picked from commit 85a78ad9e7f5b3c74eb84949d3f3e53bf4d9fdd6)
    
    Trying to become a little more pli-ish
    Rework lib/python/Screens/StartWizard.py and tools/enigma2.sh.in - this
    will cause damage and needs to be fixed later after further merging

--- lib/python/Screens/StartWizard.py
+++ lib/python/Screens/StartWizard.py
@@ -1,4 +1,5 @@
 from Screens.Wizard import wizardManager
+from Screens.Screen import Screen
 from Screens.MessageBox import MessageBox
 from Screens.WizardLanguage import WizardLanguage
 from Screens.Rc import Rc
@@ -9,13 +10,19 @@ try:
 except:
 	OverscanWizard = None
 
-from boxbranding import getBoxType
 
+from boxbranding import getBoxType
 from Components.About import about
 from Components.Pixmap import Pixmap
-from Components.config import config, ConfigBoolean, configfile
+from Components.ProgressBar import ProgressBar
+from Components.Label import Label
+from Components.ScrollLabel import ScrollLabel
 from Components.SystemInfo import SystemInfo
+from Components.config import config, ConfigBoolean, configfile
 from Screens.LanguageSelection import LanguageWizard
+from enigma import eConsoleAppContainer, eTimer, eActionMap, quitMainloop
+
+import os
 
 config.misc.firstrun = ConfigBoolean(default=True)
 config.misc.languageselected = ConfigBoolean(default=True)
@@ -59,6 +66,139 @@ def checkForDevelopImage():
 		config.misc.check_developimage.save()
 
 
+def setLanguageFromBackup(backupfile):
+	try:
+		import tarfile
+		tar = tarfile.open(backupfile)
+		for member in tar.getmembers():
+			if member.name == 'etc/enigma2/settings':
+				for line in tar.extractfile(member):
+					line = line.decode()
+					if line.startswith('config.osd.language'):
+						languageToSelect = line.strip().split('=')[1]
+						if languageToSelect:
+							language.activateLanguage(languageToSelect)
+							break
+		tar.close()
+	except:
+		pass
+
+
+def checkForAvailableAutoBackup():
+	for backupfile in ["/media/%s/backup/PLi-AutoBackup.tar.gz" % media for media in os.listdir("/media/") if os.path.isdir(os.path.join("/media/", media))]:
+		if os.path.isfile(backupfile):
+			setLanguageFromBackup(backupfile)
+			return True
+
+
+class AutoRestoreWizard(MessageBox):
+	def __init__(self, session):
+		MessageBox.__init__(self, session, _("Do you want to autorestore settings?"), type=MessageBox.TYPE_YESNO, timeout=20, default=True, simple=True)
+
+	def close(self, value):
+		if value:
+			if os.path.isfile("/etc/.doNotAutoInstall"):
+				os.unlink("/etc/.doNotAutoInstall")
+				MessageBox.close(self, 43)
+			else:
+				self.session.open(AutoInstall)
+		MessageBox.close(self)
+
+
+class AutoInstall(Screen):
+	skin = """<screen name="AutoInstall" position="fill" flags="wfNoBorder">
+		<panel position="left" size="5%,*"/>
+		<panel position="right" size="5%,*"/>
+		<panel position="top" size="*,5%"/>
+		<panel position="bottom" size="*,5%"/>
+		<widget name="header" position="top" size="*,48" font="Regular;38" noWrap="1"/>
+		<widget name="progress" position="top" size="*,24" backgroundColor="#00242424"/>
+		<eLabel position="top" size="*,2"/>
+		<widget name="AboutScrollLabel" font="Fixed;20" position="fill"/>
+	</screen>"""
+
+	def __init__(self, session):
+		Screen.__init__(self, session)
+		self["progress"] = ProgressBar()
+		self["progress"].setRange((0, 100))
+		self["progress"].setValue(0)
+		self["AboutScrollLabel"] = ScrollLabel("", showscrollbar=False)
+		self["header"] = Label(_("Autoinstalling please wait for packages being updated"))
+
+		self.logfile = open('/home/root/autoinstall.log', 'w')
+		self.container = eConsoleAppContainer()
+		self.container.appClosed.append(self.appClosed)
+		self.container.dataAvail.append(self.dataAvail)
+		self.package = None
+
+		import glob
+		mac_address = open('/sys/class/net/eth0/address', 'r').readline().strip().replace(":", "")
+		autoinstallfiles = glob.glob('/media/*/backup/autoinstall%s' % mac_address) + glob.glob('/media/net/*/backup/autoinstall%s' % mac_address)
+		if not autoinstallfiles:
+			autoinstallfiles = glob.glob('/media/*/backup/autoinstall') + glob.glob('/media/net/*/backup/autoinstall')
+		autoinstallfiles.sort(key=os.path.getmtime, reverse=True)
+		for autoinstallfile in autoinstallfiles:
+			if os.path.isfile(autoinstallfile):
+				autoinstalldir = os.path.dirname(autoinstallfile)
+				self.packages = [package.strip() for package in open(autoinstallfile).readlines()] + [os.path.join(autoinstalldir, file) for file in os.listdir(autoinstalldir) if file.endswith(".ipk")]
+				if self.packages:
+					self.number_of_packages = len(self.packages)
+					# make sure we have a valid package list before attempting to restore packages
+					self.container.execute("opkg update")
+					return
+
+		self.abort()
+
+	def run_console(self):
+		self["progress"].setValue(100 * (self.number_of_packages - len(self.packages)) / self.number_of_packages)
+		try:
+			open("/proc/progress", "w").write(str(self["progress"].value))
+		except IOError:
+			pass
+		self.package = self.packages.pop(0)
+		self["header"].setText(_("Autoinstalling %s") % self.package + " - %s%%" % self["progress"].value)
+		try:
+			if self.container.execute('opkg install "%s"' % self.package):
+				raise Exception("failed to execute command!")
+				self.appClosed(True)
+		except Exception as e:
+			self.appClosed(True)
+
+	def dataAvail(self, data):
+		if isinstance(data, bytes):
+			data = data.decode()
+		self["AboutScrollLabel"].appendText(data)
+		self.logfile.write(data)
+
+	def appClosed(self, retval=False):
+		if retval:
+			if self.package:
+				self.dataAvail("An error occurred during installing %s - Please try again later\n" % self.package)
+			else:
+				self.dataAvail("An error occurred during opkg update - Please try again later\n")
+		installed = [line.strip().split(":", 1)[1].strip() for line in open('/var/lib/opkg/status').readlines() if line.startswith('Package:')]
+		self.packages = [package for package in self.packages if package not in installed]
+		if self.packages:
+			self.run_console()
+		else:
+			self["progress"].setValue(100)
+			self["header"].setText(_("Autoinstalling Completed"))
+			self.delay = eTimer()
+			self.delay.callback.append(self.abort)
+			eActionMap.getInstance().bindAction('', 0, self.abort)
+			self.delay.startLongTimer(5)
+
+	def abort(self, key=None, flag=None):
+		if hasattr(self, 'delay'):
+			self.delay.stop()
+			eActionMap.getInstance().unbindAction('', self.abort)
+			self.container.appClosed.remove(self.appClosed)
+			self.container.dataAvail.remove(self.dataAvail)
+		self.container = None
+		self.logfile.close()
+		quitMainloop(43)
+
+
 class DevelopWizard(MessageBox):
 	def __init__(self, session):
 		MessageBox.__init__(self, session, _("This image is intended for developers and testers.\nNo support will be provided!\nDo you understand this?"), type=MessageBox.TYPE_YESNO, timeout=20, default=False, simple=True)
@@ -85,6 +225,11 @@ class LanguageDeleteWizard(MessageBox):
 		MessageBox.close(self)
 
 
+if not os.path.isfile("/etc/installed"):
+	from Components.Console import Console
+	Console().ePopen("opkg list_installed | cut -d ' ' -f 1 > /etc/installed;chmod 444 /etc/installed")
+
+wizardManager.registerWizard(AutoRestoreWizard, config.misc.languageselected.value and config.misc.firstrun.value and checkForAvailableAutoBackup(), priority=0)
 wizardManager.registerWizard(DevelopWizard, checkForDevelopImage(), priority=0)
 wizardManager.registerWizard(LanguageWizard, config.misc.languageselected.value, priority=10)
 wizardManager.registerWizard(LanguageDeleteWizard, config.misc.ask_languagedeletion.value, priority=10)
