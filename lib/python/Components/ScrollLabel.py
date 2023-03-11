323307844876b71a3627eeba6ad9c9dad75a36e4
Rowland Creed <rowland@moralet.com>
2023-03-11 23:25:12 +0100

    [ScrollLabel] add possibility for callbacks
    (cherry picked from commit 704afdde2fd24a668e6a4dcd73b3a4cdd8732d52)

--- lib/python/Components/ScrollLabel.py
+++ lib/python/Components/ScrollLabel.py
@@ -19,6 +19,7 @@ class ScrollLabel(GUIComponent):
 		self.column = 0
 		self.split = False
 		self.splitchar = "|"
+		self.onSelectionChanged = []
 
 	def applySkin(self, desktop, parent):
 		scrollbarWidth = 20
@@ -87,6 +88,7 @@ class ScrollLabel(GUIComponent):
 		self.curPos = max(0, min(pos, self.TotalTextHeight - self.pageHeight))
 		self.long_text.move(ePoint(0, -self.curPos))
 		self.split and self.right_text.move(ePoint(self.column, -self.curPos))
+		self.selectionChanged()
 
 	def setText(self, text, showBottom=False):
 		self.message = text
@@ -161,3 +163,7 @@ class ScrollLabel(GUIComponent):
 
 	def getText(self):
 		return self.message
+
+	def selectionChanged(self):
+		for x in self.onSelectionChanged:
+			x()
