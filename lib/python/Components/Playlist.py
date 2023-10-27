04db8cb61cf4219d54173f3bfca2296dcf105334
WanWizard <wanwizard@wanwizard.eu>
2023-10-27 14:21:03 +0100

    skip non-utf8 entries when loading a playlist
    (cherry picked from commit 94e6b6b03b012f0df7401bc75600ea37b1d8655a)

--- lib/python/Components/Playlist.py
+++ lib/python/Components/Playlist.py
@@ -51,10 +51,13 @@ class PlaylistIOInternal(PlaylistIO):
 		except IOError:
 			return None
 		while True:
-			entry = _file.readline().strip()
-			if entry == "":
-				break
-			self.addService(ServiceReference(entry))
+			try:
+				entry = _file.readline().strip()
+				if entry == "":
+					break
+				self.addService(ServiceReference(entry))
+			except UnicodeDecodeError:
+				pass
 		_file.close()
 		return self.list
 
