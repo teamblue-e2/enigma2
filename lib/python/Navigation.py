b781778a5c2a4e03d212f4d58566c661c0580114
Dimitar Tsenev <dimitar_tsenev@outlook.com>
2023-11-30 16:28:32 +0200

    Add a handling of "No free tuner" message when zap out from stream relay service (#3852)
    * [Navigation] simplify streamrelay shutdown timer
    
    * [Nav] update SR delay
    
    * [Fixed] Wrong ident
    
    (cherry picked from commit f7191f3e72d489aed53972c292af45eadbfbc98d)

--- lib/python/Navigation.py
+++ lib/python/Navigation.py
@@ -16,7 +16,7 @@ import NavigationInstance
 import ServiceReference
 from Screens.InfoBar import InfoBar
 from Components.Sources.StreamService import StreamServiceList
-from Screens.InfoBarGenerics import streamrelayChecker
+from Screens.InfoBarGenerics import streamrelayChecker, whitelist
 
 # TODO: remove pNavgation, eNavigation and rewrite this stuff in python.
 
@@ -51,6 +51,8 @@ class Navigation:
 		self.__wasTimerWakeup = False
 		self.__wasRecTimerWakeup = False
 		self.syncCount = 0
+		self.currentServiceIsStreamRelay = False
+		self.skipServiceReferenceReset = False
 		self.__isRestartUI = config.misc.RestartUI.value
 		startup_to_standby = config.usage.startup_to_standby.value
 		wakeup_time_type = config.misc.prev_wakeup_time_type.value
@@ -256,8 +258,16 @@ class Navigation:
 								if config.usage.frontend_priority_dvbs.value != config.usage.frontend_priority.value:
 									setPreferredTuner(int(config.usage.frontend_priority_dvbs.value))
 									setPriorityFrontend = True
-				if self.pnav.playService(playref):
-					print("[Navigation] Failed to start: ", playref.toString())
+				if config.misc.softcam_streamrelay_delay.value and self.currentServiceIsStreamRelay:
+					self.currentServiceIsStreamRelay = False
+					self.currentlyPlayingServiceReference = None
+					self.currentlyPlayingServiceOrGroup = None
+					print("[Navigation] Streamrelay was active -> delay the zap till tuner is freed")
+					self.retryServicePlayTimer = eTimer()
+					self.retryServicePlayTimer.callback.append(boundFunction(self.playService, ref, checkParentalControl, forceRestart, adjust))
+					self.retryServicePlayTimer.start(config.misc.softcam_streamrelay_delay.value, True)
+				elif self.pnav.playService(playref):
+					# print("[Navigation] Failed to start", playref)
 					self.currentlyPlayingServiceReference = None
 					self.currentlyPlayingServiceOrGroup = None
 					if oldref and "://" in oldref.getPath():
@@ -268,6 +278,8 @@ class Navigation:
 				self.skipServiceReferenceReset = False
 				if setPriorityFrontend:
 					setPreferredTuner(int(config.usage.frontend_priority.value))
+				if self.currentlyPlayingServiceReference and self.currentlyPlayingServiceReference.toString() in whitelist.streamrelay:
+					self.currentServiceIsStreamRelay = True
 				return 0
 		elif oldref and InfoBarInstance and InfoBarInstance.servicelist.servicelist.setCurrent(oldref, adjust):
 			self.currentlyPlayingServiceOrGroup = InfoBarInstance.servicelist.servicelist.getCurrent()
