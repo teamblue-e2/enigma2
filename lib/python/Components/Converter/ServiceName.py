a1c1bc602a0b1f21a5e4fbda6f4b46d44d71b45e
Dimitar Tsenev <dimitar_tsenev@outlook.com>
2023-12-03 18:49:29 +0200

    [Updated] Added tuner system to ServiceName converter
    (cherry picked from commit 57544eb9c26deec364b966026fe9647e1aab7d61)

--- lib/python/Components/Converter/ServiceName.py
+++ lib/python/Components/Converter/ServiceName.py
@@ -2,7 +2,9 @@
 from Components.Converter.Converter import Converter
 from enigma import iServiceInformation, iPlayableService, iPlayableServicePtr, eServiceReference
 from ServiceReference import resolveAlternate
+from Components.config import config
 from Components.Element import cached
+from Tools.Transponder import ConvertToHumanReadable
 
 
 class ServiceName(Converter):
@@ -58,22 +60,24 @@ class ServiceName(Converter):
 			numservice = self.source.serviceref
 			return self.getNumber(numservice, info)
 		elif self.type == self.FORMAT_STRING:
-			name = self.getName(ref, info)
-			numservice = self.source.serviceref
-			num = self.getNumber(numservice, info)
-			orbpos, tp_data = self.getOrbitalPos(ref, info)
-			provider = self.getProvider(ref, info, tp_data)
+			name = self.getName(service, info)
+			numservice = hasattr(self.source, "serviceref") and self.source.serviceref
+			num = numservice and self.getNumber(numservice, info) or ""
+			orbpos, tp_data = self.getOrbitalPos(service, info)
+			provider = self.getProvider(service, info, tp_data)
+			tuner_system = self.getServiceSystem(service, info, tp_data)
 			res_str = ""
 			for x in self.parts[1:]:
-				x = x.upper()
 				if x == "NUMBER" and num:
 					res_str = self.appendToStringWithSeparator(res_str, num)
 				if x == "NAME" and name:
 					res_str = self.appendToStringWithSeparator(res_str, name)
 				if x == "ORBPOS" and orbpos:
 					res_str = self.appendToStringWithSeparator(res_str, orbpos)
-				if x == "PROVIDER" and provider is not None and provider:
+				if x == "PROVIDER" and provider:
 					res_str = self.appendToStringWithSeparator(res_str, provider)
+				if x == "TUNERSYSTEM" and tuner_system:
+					res_str = self.appendToStringWithSeparator(res_str, tuner_system)
 			return res_str
 
 
@@ -99,8 +103,8 @@ class ServiceName(Converter):
 		else:
 			num = str(num)
 		return num
-	
-	def getProvider(self, ref, info):
+
+	def getProvider(self, ref, info, tp_data=None):
 		if ref:
 			return info.getInfoString(ref, iServiceInformation.sProvider)
 		return info.getInfoString(iServiceInformation.sProvider)
@@ -111,13 +115,36 @@ class ServiceName(Converter):
 			tp_data = info.getInfoObject(ref, iServiceInformation.sTransponderData)
 		else:
 			tp_data = info.getInfoObject(iServiceInformation.sTransponderData)
+
+		if not tp_data and not ref:
+			service = self.source.service
+			if service:
+				feraw = service.frontendInfo()
+				tp_data = feraw and feraw.getAll(config.usage.infobar_frontend_source.value == "settings")
+
 		if tp_data is not None:
 			try:
 				position = tp_data["orbital_position"]
 				if position > 1800: # west
-					orbitalpos = "%.1f " %(float(3600 - position)/10) + _("W")
+					orbitalpos = "%.1f " %(float(3600 - position)/10) + _("°W")
 				else:
-					orbitalpos = "%.1f " %(float(position)/10) + _("E")
+					orbitalpos = "%.1f " %(float(position)/10) + _("°E")
 			except:
 				pass
-		return orbitalpos
+		return orbitalpos, tp_data
+	
+	def getServiceSystem(self, ref, info, feraw):
+		if ref:
+			sref = info.getInfoObject(ref, iServiceInformation.sServiceref)
+		else:
+			sref = info.getInfoObject(iServiceInformation.sServiceref)
+		
+		if not sref:
+			sref = ref.toString()
+			
+		if sref and "%3a//" in sref:
+			return "IPTV"
+			
+		fedata = ConvertToHumanReadable(feraw)
+
+		return fedata.get("system") or ""
