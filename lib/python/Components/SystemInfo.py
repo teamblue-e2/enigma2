02f255c954860877b04528ddce23d9eed2457360
Littlesat <littlesat@openpli.org>
2024-01-04 16:44:38 +0100

    Remove the immutable work-a-rounds from BoxInfo.
    As actually it does not have a real added value as I also discussed with
    Huevos. Read e.g. the discussion here: https://forums.openpli.org/topic/97296-boxinfo-discussion/page-5
    It is possible to add complicated code but at the end with
    BoxInfo.boxInfo = {} you can clear all and/or overwrite everything. You
    only try to protect when someone with coding does something by accident.
    By not checking this the code looks very lean and both worlds SystemInfo
    and BoxInfo work and are fully in Sync. So when someone changed a key in
    SystemInfo, BoxInfo gives the same result and visa versa.
    
    (cherry picked from commit 09f0482426407c489359ffa7c0f45a805435e960)

--- lib/python/Components/SystemInfo.py
+++ lib/python/Components/SystemInfo.py
@@ -1,6 +1,5 @@
 import re
 from hashlib import md5
-from types import MappingProxyType
 from ast import literal_eval
 from enigma import Misc_Options, eDVBCIInterfaces, eDVBResourceManager, eGetEnigmaDebugLvl
 from Tools.Directories import SCOPE_PLUGINS, SCOPE_SKIN, fileCheck, fileExists, fileHas, pathExists, resolveFilename, isPluginInstalled
@@ -9,29 +8,26 @@ from boxbranding import getBoxType, getMachineBuild, getBrandOEM, getMachineMtdR
 
 class BoxInformation:
 	def __init__(self, root=""):
-		boxInfoCollector = {"machine": "default"} #add one key to the boxInfoCollector as it always should exist to satisfy the CI test on github
-		self.boxInfoMutable = {}
-		boxInfoCollector["checksum"] = None
+		self.boxInfo = {"machine": "default", "checksum": None} #add one key to the boxInfoCollector as it always should exist to satisfy the CI test on github and predefine checksum
 		checksumcollectionstring = ""
 		file = root + "/usr/lib/enigma.info"
 		if fileExists(file):
 			for line in open(file, 'r').readlines():
 				if line.startswith("checksum="):
-					boxInfoCollector["checksum"] = md5(bytearray(checksumcollectionstring, "UTF-8", errors="ignore")).hexdigest() == line.strip().split('=')[1]
+					self.boxInfo["checksum"] = md5(bytearray(checksumcollectionstring, "UTF-8", errors="ignore")).hexdigest() == line.strip().split('=')[1]
 					break
 				checksumcollectionstring += line
 				if line.startswith("#") or line.strip() == "":
 					continue
 				if '=' in line:
 					item, value = [x.strip() for x in line.split('=')]
-					boxInfoCollector[item] = self.processValue(value)
-			if boxInfoCollector["checksum"]:
+					self.boxInfo[item] = self.processValue(value)
+			if self.boxInfo["checksum"]:
 				print("[SystemInfo] Enigma information file data loaded into BoxInfo.")
 			else:
 				print("[SystemInfo] Enigma information file data loaded, but checksum failed.")
 		else:
 			print("[SystemInfo] ERROR: %s is not available!  The system is unlikely to boot or operate correctly." % file)
-		self.boxInfo = MappingProxyType(boxInfoCollector)
 
 	def processValue(self, value):
 		try:
@@ -58,34 +54,15 @@ class BoxInformation:
 	def getItem(self, item, default=None):
 		if item in self.boxInfo:
 			return self.boxInfo[item]
-		elif item in self.boxInfoMutable:
-			return self.boxInfoMutable[item]
 		return default
 
-	def setItem(self, item, value, immutable=False, forceOverride=False):
-		if item in self.boxInfo and not forceOverride:
-			print("[BoxInfo] Error: Item '%s' is immutable and can not be %s!" % (item, "changed" if item in self.boxInfo else "added"))
-			return False
-		if immutable:
-			boxInfoCollector = dict(self.boxInfo)
-			boxInfoCollector[item] = value
-			self.boxInfo = MappingProxyType(boxInfoCollector)
-		else:
-			self.boxInfoMutable[item] = value
+	def setItem(self, item, value, *args, **kws):
+		self.boxInfo[item] = value
 		return True
 
-	def deleteItem(self, item, forceOverride=False):
-		if item in self.boxInfo:
-			if forceOverride:
-				boxInfoCollector = dict(self.boxInfo)
-				del boxInfoCollector[item]
-				self.boxInfo = MappingProxyType(boxInfoCollector)
-				return True
-			else:
-				print("[BoxInfo] Error: Item '%s' is immutable and can not be deleted!" % item)
-		if item in self.boxInfoMutable:
-			del self.boxInfoMutable[item]
-			return True
+	def deleteItem(self, item, *args, **kws):
+		del self.boxInfo[item]
+		return True
 
 
 BoxInfo = BoxInformation()
@@ -128,8 +105,7 @@ def getBootdevice():
 	return dev
 
 #This line makes the new BoxInfo backwards compatible with SystemInfo without duplicating the dictionary.
-#As soon anywhere in enigma2 and all the plugins SystemInfo is not used anymore it can be considered to remove this line
-SystemInfo = BoxInfo.boxInfoMutable
+SystemInfo = BoxInfo.boxInfo
 from Tools.Multiboot import getMultibootStartupDevice, getMultibootslots  # This import needs to be here to avoid a SystemInfo load loop!
 SystemInfo["HasRootSubdir"] = False	# This needs to be here so it can be reset by getMultibootslots!
 SystemInfo["RecoveryMode"] = False or fileCheck("/proc/stb/fp/boot_mode")	# This needs to be here so it can be reset by getMultibootslots!
@@ -259,3 +235,4 @@ SystemInfo["HasPhysicalLoopthrough"] = ["Vuplus DVB-S NIM(AVL2108)", "GIGA DVB-S
 SystemInfo["HasFBCtuner"] = ["Vuplus DVB-C NIM(BCM3158)", "Vuplus DVB-C NIM(BCM3148)", "Vuplus DVB-S NIM(7376 FBC)", "Vuplus DVB-S NIM(45308X FBC)", "Vuplus DVB-S NIM(45208 FBC)", "DVB-S2 NIM(45208 FBC)", "DVB-S2X NIM(45308X FBC)", "DVB-S2 NIM(45308 FBC)", "DVB-C NIM(3128 FBC)", "BCM45208", "BCM45308X", "BCM3158"]
 SystemInfo["HasHiSi"] = pathExists("/proc/hisi")
 SystemInfo["FCCactive"] = False
+SystemInfo["Autoresolution_proc_videomode"] = model in ("gbue4k", "gbquad4k") and "/proc/stb/video/videomode_50hz" or "/proc/stb/video/videomode"
