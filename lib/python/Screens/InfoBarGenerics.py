c42771f1eeac4b6726620160e2040d079b7e7062
Littlesat <littlesat@openpli.org>
2024-01-21 15:32:11 +0100

    Prepare userinterface for PIN userbouquet functionality
    At this moment it actually does nothing but you can select usuerbouquets
    you would like to pin. I needed for some future testing. When the
    functionality really does not help this commit can be reverted.
    
    (cherry picked from commit 755bd4bde4a6d15226926f82cbfe8e7b303e6eed)

--- lib/python/Screens/InfoBarGenerics.py
+++ lib/python/Screens/InfoBarGenerics.py
@@ -151,12 +151,16 @@ resumePointCacheLast = int(time())
 
 class whitelist:
 	vbi = []
+	bouquets = []
 
 def reload_whitelist_vbi():
 	whitelist.vbi = [line.strip() for line in open('/etc/enigma2/whitelist_vbi', 'r').readlines()] if os.path.isfile('/etc/enigma2/whitelist_vbi') else []
 
+def reload_whitelist_bouquets():
+	whitelist.bouquets = [line.strip() for line in open('/etc/enigma2/whitelist_bouquets', 'r').readlines()] if os.path.isfile('/etc/enigma2/whitelist_bouquets') else []
 
 reload_whitelist_vbi()
+reload_whitelist_bouquets()
 
 class InfoBarStreamRelay:
 
@@ -584,6 +588,9 @@ class InfoBarShowHide(InfoBarScreenSaver):
 					return ".hidevbi." in servicepath.lower()
 		return service and service.toString() in whitelist.vbi
 
+	def checkBouquets(self, bouquet):
+		return bouquet in whitelist.bouquets
+
 	def checkStreamrelay(self, service):
 		return streamrelay.checkService(service)
 
@@ -603,6 +610,14 @@ class InfoBarShowHide(InfoBarScreenSaver):
 				whitelist.vbi.append(service)
 			open('/etc/enigma2/whitelist_vbi', 'w').write('\n'.join(whitelist.vbi))
 			self.showHideVBI()
+
+	def ToggleBouquet(self, bouquet):
+		if bouquet in whitelist.bouquets:
+			whitelist.bouquets.remove(bouquet)
+		else:
+			whitelist.bouquets.append(bouquet)
+		open('/etc/enigma2/whitelist_bouquets', 'w').write('\n'.join(whitelist.bouquets))
+
 
 class BufferIndicator(Screen):
 	def __init__(self, session):
