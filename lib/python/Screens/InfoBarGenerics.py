e4a42176f591611a7aad8214f0d7e474b57d7f28
teamblue-e2 <teamblue@online.de>
2024-06-04 18:13:10 +0200

    [InfoBarGenerics] enable SoftcamSetup in extension menu

--- lib/python/Screens/InfoBarGenerics.py
+++ lib/python/Screens/InfoBarGenerics.py
@@ -2506,16 +2506,16 @@ class InfoBarExtensions:
 
 	def __init__(self):
 		self.list = []
-		# self.addExtension((lambda: _("Softcam Setup"), self.openSoftcamSetup, lambda: config.misc.softcam_setup.extension_menu.value and BoxInfo.getItem("HasSoftcamInstalled")), "1")
+		self.addExtension((lambda: _("Softcam Setup"), self.openSoftcamSetup, lambda: config.misc.softcam_setup.extension_menu.value and BoxInfo.getItem("HasSoftcamInstalled")), "1")
 		self.addExtension((lambda: _("Manually import from fallback tuner"), self.importChannels, lambda: config.usage.remote_fallback_extension_menu.value and config.usage.remote_fallback_import.value))
 		self["InstantExtensionsActions"] = HelpableActionMap(self, ["InfobarExtensions"],
 			{
 				"extensions": (self.showExtensionSelection, _("Show extensions...")),
 			}, 1) # lower priority
 
-	#def openSoftcamSetup(self):
-	#	from Screens.SoftcamSetup import SoftcamSetup
-	#	self.session.open(SoftcamSetup)
+	def openSoftcamSetup(self):
+		from Screens.SoftcamSetup import SoftcamSetup
+		self.session.open(SoftcamSetup)
 
 	def importChannels(self):
 		from Components.ImportChannels import ImportChannels
