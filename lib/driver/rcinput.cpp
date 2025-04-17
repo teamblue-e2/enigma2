c8737377d758a18c1b493b890c8a364ed8aaaa32
teamblue-e2 <teamblue@online.de>
2025-04-17 10:15:33 +0200

    Try to fix BT RCU problems

--- lib/driver/rcinput.cpp
+++ lib/driver/rcinput.cpp
@@ -42,7 +42,7 @@ void eRCDeviceInputDev::handleCode(long rccode)
 	if (km == eRCInput::kmAscii)
 	{
 		bool ignore = false;
-		bool ascii = ev->code > 0 && ev->code < 59;
+		bool ascii = (ev->code > 0 && ev->code < 61);
 
 		switch (ev->code)
 		{
@@ -249,6 +249,14 @@ public:
 
 	void add(const char* filename)
 	{
+		for (itemlist::iterator it = items.begin(); it != items.end(); ++it)
+		{
+			if (strcmp((*it)->filename, filename) == 0)
+			{
+				// Ignore if already exists
+				return;
+			}
+		}
 		eDebug("[eInputDeviceInit] adding device %s", filename);
 		eRCInputEventDriver *p = new eRCInputEventDriver(filename);
 		items.push_back(new element(filename, p, new eRCDeviceInputDev(p, consoleFd)));
