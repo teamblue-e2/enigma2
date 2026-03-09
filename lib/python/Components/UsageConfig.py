c5556e3a56c4c0d36b3042bd353fe87032744441
teamblue-e2 <teamblue@online.de>
2026-03-09 16:33:44 +0100

    Fix ident

--- lib/python/Components/UsageConfig.py
+++ lib/python/Components/UsageConfig.py
@@ -986,7 +986,7 @@ def InitUsageConfig():
 	config.misc.softcam_streamrelay_port = ConfigInteger(default=17999, limits=(0, 65535))
 	config.misc.softcam_streamrelay_delay = ConfigSelectionNumber(min=0, max=2000, stepwidth=50, default=100, wraparound=True)
 
-    defaultValue = 1 if BoxInfo.getItem('model') in ("gb7252", ) else 0
+	defaultValue = 1 if BoxInfo.getItem('model') in ("gb7252", ) else 0
 	config.softcsa = ConfigSubsection()
 	config.softcsa.decoderRelease = ConfigSelection(default=0, choices=[
 		(0, _("Quick")),
