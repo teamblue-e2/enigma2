4542dee0e57f5fda303e08b30f9d2f6fdc84510d
Hains van den Bosch <hainsvdbosch@ziggo.nl>
2026-04-26 13:15:11 +0200

    [lcd.cpp] Fix build with Wrynose branch
    Fix:
    
    | ../../sources/enigma2-3.14+git/lib/gdi/lcd.cpp: In member function 'virtual void eDBoxLCD::update()':
    | ../../sources/enigma2-3.14+git/lib/gdi/lcd.cpp:247:22: error: ignoring return value of '
    ssize_t write(int, const void*, size_t)' declared with attribute 'warn_unused_result' [-Werror=unused-result]
    |   247 |                 write(lcdfd, raw, 132 * 8);
    |       |                 ~~~~~^~~~~~~~~~~~~~~~~~~~~
    | ../../sources/enigma2-3.14+git/lib/gdi/lcd.cpp:268:30: error: ignoring return value of
    'ssize_t write(int, const void*, size_t)' declared with attribute 'warn_unused_result' [-Werror=unused-result]
    |   268 |                         write(lcdfd, raw, _stride * height);
    |       |                         ~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    
    (cherry picked from commit 3c9ba79eafbc6a23a58e85d61ab2660cc330139e)

--- lib/gdi/lcd.cpp
+++ lib/gdi/lcd.cpp
@@ -302,7 +302,9 @@ void eDBoxLCD::update()
 					raw[y * 132 + x] = pix ^ inverted;
 			}
 		}
-		write(lcdfd, raw, 132 * 8);
+		if (write(lcdfd, raw, 132 * 8) == -1) {
+			eDebug("[eDboxLCD] write() (1) failed");
+		}
 	}
 	else if (lcd_type == 3)
 	{
@@ -323,7 +325,9 @@ void eDBoxLCD::update()
 						raw[y * width + x] = _buffer[y * width + x] ^ inverted;
 				}
 			}
-			write(lcdfd, raw, _stride * height);
+			if (write(lcdfd, raw, _stride * height) == -1) {
+				eDebug("[eDboxLCD] write() (2) failed");
+			}
 		}
 		else
 		{
@@ -359,7 +363,9 @@ void eDBoxLCD::update()
 			else
 			{
 #endif
-				write(lcdfd, _buffer, _stride * res.height());
+			if (write(lcdfd, _buffer, _stride * res.height()) == -1) {
+				eDebug("[eDboxLCD] write() (3) failed");
+			}
 #ifdef LCD_COLOR_BITORDER_RGB565
 			}
 #endif
@@ -390,7 +396,9 @@ void eDBoxLCD::update()
 					raw[y * 64 + x] = pix;
 			}
 		}
-		write(lcdfd, raw, 64 * 64);
+		if (write(lcdfd, raw, 64 * 64) == -1) {
+			eDebug("[eDboxLCD] write() (4) failed");
+		}
 	}
 	dumpLCD2PNG();
 #endif
