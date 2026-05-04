9b1a600cbd975029ccaa83e9fd31962a323d10f1
teamblue-e2 <teamblue@online.de>
2026-05-04 10:14:16 +0200

    [NetworkBrowser] Add NetworkBrowser to menu

--- lib/python/Screens/NetworkSetup.py
+++ lib/python/Screens/NetworkSetup.py
@@ -1559,6 +1559,107 @@ class NetworkAdapterTest(Screen):
 			iStatus.stopWlanConsole()
 
 
+class NetworkMountsMenu(Screen, HelpableScreen):
+	def __init__(self, session):
+		Screen.__init__(self, session)
+		HelpableScreen.__init__(self)
+		self.setTitle(_("Mounts"))
+		self.session = session
+		self.onChangedEntry = []
+		self.mainmenu = self.genMainMenu()
+		self["menulist"] = MenuList(self.mainmenu)
+		self["key_red"] = StaticText(_("Close"))
+		self["introduction"] = StaticText()
+
+		self["WizardActions"] = HelpableActionMap(self, "WizardActions",
+		{
+			"up": (self.up, _("Move up to previous entry")),
+			"down": (self.down, _("Move down to next entry")),
+			"left": (self.left, _("Move up to first entry")),
+			"right": (self.right, _("Move down to last entry")),
+		})
+
+		self["OkCancelActions"] = HelpableActionMap(self, "OkCancelActions",
+		{
+			"cancel": (self.close, _("Exit mounts setup menu")),
+			"ok": (self.ok, _("Select menu entry")),
+		})
+
+		self["ColorActions"] = HelpableActionMap(self, "ColorActions",
+		{
+			"red": (self.close, _("Exit networkadapter setup menu")),
+		})
+
+		self["actions"] = NumberActionMap(["WizardActions", "ShortcutActions"],
+		{
+			"ok": self.ok,
+			"back": self.close,
+			"up": self.up,
+			"down": self.down,
+			"red": self.close,
+			"left": self.left,
+			"right": self.right,
+		}, -2)  # noqa: E123
+
+		if self.selectionChanged not in self["menulist"].onSelectionChanged:
+			self["menulist"].onSelectionChanged.append(self.selectionChanged)
+		self.selectionChanged()
+
+	def createSummary(self):
+		from Screens.PluginBrowser import PluginBrowserSummary
+		return PluginBrowserSummary
+
+	def selectionChanged(self):
+		item = self["menulist"].getCurrent()
+		if item:
+			if item[1][0] == "extendedSetup":
+				self["introduction"].setText(_(item[1][1]))
+			name = str(self["menulist"].getCurrent()[0])
+			desc = self["introduction"].text
+		else:
+			name = ""
+			desc = ""
+		for cb in self.onChangedEntry:
+			cb(name, desc)
+
+	def ok(self):
+		if self["menulist"].getCurrent()[1][0] == "extendedSetup":
+			self.extended = self["menulist"].getCurrent()[1][2]
+			self.extended(self.session)
+
+	def up(self):
+		self["menulist"].up()
+
+	def down(self):
+		self["menulist"].down()
+
+	def left(self):
+		self["menulist"].pageUp()
+
+	def right(self):
+		self["menulist"].pageDown()
+
+	def genMainMenu(self):
+		menu = []
+		self.extended = None
+		self.extendedSetup = None
+		for p in plugins.getPlugins(PluginDescriptor.WHERE_NETWORKMOUNTS):
+			callFnc = p.fnc["ifaceSupported"](self)
+			if callFnc is not None:
+				self.extended = callFnc
+				if "menuEntryName" in p.fnc:
+					menuEntryName = p.fnc["menuEntryName"](self)
+				else:
+					menuEntryName = _("Extended Setup...")
+				if "menuEntryDescription" in p.fnc:
+					menuEntryDescription = p.fnc["menuEntryDescription"](self)
+				else:
+					menuEntryDescription = _("Extended Networksetup Plugin...")
+				self.extendedSetup = ("extendedSetup", menuEntryDescription, self.extended)
+				menu.append((menuEntryName, self.extendedSetup))
+		return menu
+
+
 class NetworkPassword(ConfigListScreen, Screen):
 	def __init__(self, session):
 		Screen.__init__(self, session)
